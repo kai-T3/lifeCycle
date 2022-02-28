869bf7e86b8766efc20efcbe1a39c7db2fde2c44
kai-T3 <tankl-wm19@student.tarc.edu.my>
2022-02-28 14:10:36 +0800

    lifecycle

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.activitylifecycle.mymodels.MyData
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var myValue: MyData
+    //var myValue: String = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate")
+        //create an object, will not be destroyed after activity is destroyed
+        myValue = ViewModelProvider(this).get(com.example.activitylifecycle.mymodels.MyData::class.java)
+
+        val btn: Button = findViewById(R.id.btnOk)
+        val tv: TextView = findViewById(R.id.tvResult)
+
+//        //if null, first time
+//        if(savedInstanceState != null){
+//            myValue = savedInstanceState.getString("value").toString();
+//            tv.text = myValue
+//        }
+        if (myValue!= null){
+            tv.text = myValue.name
+        }
+        btn.setOnClickListener(){
+            myValue.name = "John"
+            myValue.age = 21
+            //after clicking btn OK
+            tv.text = myValue.name
+        }
+    }
+
+//    //to save data's state without losing value
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putString("value", myValue)
+//    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy")
+    }
+}
---
+++ app/src/main/java/com/example/activitylifecycle/mymodels/MyData.kt
@@ -0,0 +1,9 @@
+package com.example.activitylifecycle.mymodels
+
+import androidx.lifecycle.ViewModel
+
+class MyData: ViewModel() {
+    var name: String= ""
+    var age:Int = 0
+
+}
